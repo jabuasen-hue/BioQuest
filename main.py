11556499f14b21f825c9158c6dd855ca0ad602f3
jabuasen-hue <jabuasen@carc.pshs.edu.ph>
2026-02-22 17:17:37 +0800

    Made a main menu
    Added a main menu and made functions for each of its branches (Play, Achievements, Credits, and Quit)

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
-
+def mainMenu():
+    while True:
+        print("=======================================")
+        print("||             MAIN MENU             ||")
+        print("---------------------------------------")
+        print("| 1. Play                             |")
+        print("| 2. Achievements                     |")
+        print("| 3. Credits                          |")
+        print("| 4. Quit                             |")
+        print("---------------------------------------")
+        menuChoice = input("| What would you like to do? (1-4): ")
+        while menuChoice not in ["1", "2", "3", "4"]:
+            print("\n Please Enter a valid answer \n")
+            break
+        
+        if menuChoice == "1":
+            playOption()
+        elif menuChoice == "2":
+            achievementsOption()
+        elif menuChoice == "3":
+            creditsOption()
+        else:
+            break
+    
+def playOption():
+    while True:
+        print("\n =============== PLAY ==============")
+        print("|               TOPICS              |")
+        print("-------------------------------------")
+        print("| 1. First Quarter                  |")
+        print("| 2. Second Quarter                 |")
+        print("| 3. Third Quarter                  |")
+        print("| 4. Fourth Quarter                 |")
+        print("-------------------------------------")
+        quarterChoice = input("| Which quarter will you study/review? (1-4): ")
+        while quarterChoice not in ["1", "2", "3", "4"]:
+            print("\n Please Enter a valid answer \n")
+            break
+    
+        if quarterChoice == "1" or "2" or "3" or "4":                          
+            print("Uh oh! That isn't available yet! This will be added soon :D")
+            break               
+        
+def achievementsOption():
+    print("Uh oh! That isn't available yet! This will be added soon :D")
+    
+def creditsOption():
+    print("\n =============== CREDITS ===============")
+    print("      Program     ")
+    print(" Conceptualized By: Buasen, Julian Jesse")
+    print("                    Sanoy, Jeraiza Leigh")
+    print("                    Sagario, Julien Alexa")
+    print("                      (Honorary Mention) ")
+    print("\n Program Created By: Buasen, Julian Jesse")
+    print("                     Sanoy, Jeraiza Leigh")
+    print("\n Grade & Section: 8 - Sampaguita")
+    print(" Subject: Computer Science")
+    print(" Teacher: Ma'am Kaye Alamag")
+    print("\n - Thank you for using this program! <3 - \n")
+    
+    
+mainMenu()
